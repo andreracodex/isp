5507222dc1949cc612cde67fccfa8dca6970dabd
racodex <racodex@gmail.com>
2024-03-08 14:33:47 +0700

    update

--- app/Http/Controllers/IndoRegionController.php
+++ app/Http/Controllers/IndoRegionController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Models\Province;
 use App\Models\Regency;
 use App\Models\District;
 use App\Models\Village;
