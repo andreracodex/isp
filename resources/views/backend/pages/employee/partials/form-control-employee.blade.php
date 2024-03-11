0c7549e24107a419159a6c25c7324c2dac52f61c
anandanajahudin <ahmadananda96@gmail.com>
2024-03-11 21:01:10 +0700

    update status karyawan

--- resources/views/backend/pages/employee/partials/form-control-employee.blade.php
+++ resources/views/backend/pages/employee/partials/form-control-employee.blade.php
@@ -1,4 +1,32 @@
 <div class="row">
+    <div class="col-md-3 mb-3">
+        <div class="form-check form-switch custom-switch-v1 mt-3">
+            <input type="checkbox" class="form-check-input input-success" id="customswitchlightv1-3" name="is_active"
+                @if ($employee->is_active == 1) @checked(true) @else @checked(false) @endif>
+            <label class="form-check-label" for="customswitchlightv1-3">Karyawan Active <sup class="text-danger"
+                    data-bs-toggle="tooltip" data-bs-placement="top"
+                    data-bs-original-title="status pelanggan harap aktif, jika maasih berlangganan">*</sup></label>
+        </div>
+    </div>
+
+    <div class="col-md-9 mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none">
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path
+                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z">
+                </path>
+            </symbol>
+        </svg>
+        <div class="alert alert-warning d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24">
+                <use xlink:href="#exclamation-triangle-fill"></use>
+            </svg>
+            <div> Ini adalah pilihan untuk status karyawan Aktif / Tidak aktif</div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
     <div class="col-md-4 mb-3">
         <label class="form-label" for="nama">Nama Karyawan</label>
         <input type="text" class="form-control" name="nama_karyawan" id="nama_karyawan"
@@ -103,8 +131,8 @@
 <div class="row">
     <div class="col-md-6 mb-3">
         <label class="form-label" for="alamat_karyawan">Alamat Karyawan</label>
-        <textarea class="form-control" placeholder="Alamat Karyawan" id="exampleTextarea" name="alamat_karyawan" rows="3"
-            required>{{ old('alamat_karyawan') ?? $employee->alamat_karyawan }}</textarea>
+        <textarea class="form-control" placeholder="Alamat Karyawan" id="exampleTextarea" name="alamat_karyawan"
+            rows="3" required>{{ old('alamat_karyawan') ?? $employee->alamat_karyawan }}</textarea>
         <div class="invalid-feedback"> Harap isi alamat. </div>
     </div>
     <div class="col-md-3 mb-3">
