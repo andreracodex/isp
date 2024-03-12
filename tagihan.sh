df1af98fd12fa912a10f15cb01d7dd8ee6b1a6be
racodex <racodex@gmail.com>
2024-03-12 11:15:25 +0700

    update

--- tagihan.sh
+++ tagihan.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-clear
+# Change text color to black on light green
+echo -e "\e[32;40m"
+# Display the contents of banner.txt file
 cat banner.txt
+# Change directory to the specified path
 cd "/home/u422026669/domains/berdikari.web.id/public_html/billing/"
+# Run the PHP artisan command
 php artisan send:tagihan
+# Pause the script
+read -p "Press Enter to continue..."
