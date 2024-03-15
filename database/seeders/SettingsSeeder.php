59e1334f0fd44b7962e021eb1a39cb42cb4f2db1
racodex <racodex@gmail.com>
2024-03-15 11:32:16 +0700

    update

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -419,6 +419,22 @@ class SettingsSeeder extends Seeder
                 'user_id' => 1,
             ),
         );
+        User::insert(
+            array(
+                'name' => 'User',
+                'user_name' => 'User',
+                'email' => 'user@isp.com',
+                'is_active' => 1,
+                'password' => bcrypt(12345678),
+                'path' => '',
+                'user_type' => 'user',
+            ),
+        );
+        UserSetting::insert(
+            array(
+                'user_id' => 2,
+            ),
+        );
         // Insert Paket
         Paket::insert(
             array(
