310214ed5813fc946bad9274c288b2493d0db2b1
racodex <racodex@gmail.com>
2024-03-18 15:59:49 +0700

    update

--- app/Console/Commands/Sendtagihan.php
+++ app/Console/Commands/Sendtagihan.php
@@ -37,7 +37,20 @@ class SendTagihan extends Command
                 $orderDetail = OrderDetail::leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
                     ->where('orders.customer_id', '=', $customer->id)
                     ->orderBy('order_id', 'DESC')
-                    ->groupBy('orders.customer_id', 'due_date', 'order_details.id', 'order_details.order_id', 'order_details.uuid', 'order_details.invoice_number', 'order_details.payment_id')
+                    ->groupBy('orders.customer_id', 'order_details.due_date',
+                    'order_details.id',
+                    'order_details.order_id',
+                    'order_details.uuid',
+                    'order_details.invoice_number',
+                    'order_details.payment_id',
+                    'order_details.pay_image',
+                    'order_details.pay_description',
+                    'order_details.diskon',
+                    'order_details.biaya_admin',
+                    'order_details.ppn',
+                    'order_details.is_payed',
+                    'order_details.is_active',
+                    )
                     ->first();
 
                 $newDueDate = Carbon::parse($orderDetail->due_date)->addMonth(1)->format('Y-m-d');
@@ -46,15 +59,15 @@ class SendTagihan extends Command
                     'due_date' => $newDueDate,
                     'payment_id' => $orderDetail->payment_id,
                     'order_id' => $orderDetail->id,
-                    'invoice_number' => 'INV'.rand(11111111, 99999999),
+                    'invoice_number' => 'INV' . rand(11111111, 99999999),
                     'uuid' => Str::uuid(64),
                 ]);
 
                 $belum = OrderDetail::leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
-                ->leftJoin('pakets', 'pakets.id', '=', 'orders.paket_id')
-                ->where('orders.customer_id', '=', $customer->id)
-                ->where('is_payed', 0)
-                ->sum('pakets.harga_paket');
+                    ->leftJoin('pakets', 'pakets.id', '=', 'orders.paket_id')
+                    ->where('orders.customer_id', '=', $customer->id)
+                    ->where('is_payed', 0)
+                    ->sum('pakets.harga_paket');
 
                 Mail::to($customer->user->email)->send(new OrderCreated($customer));
                 $invoice_real = $invoice->invoice_number;
@@ -62,16 +75,16 @@ class SendTagihan extends Command
                 $message = "*Yth Pelanggan GNET*\n\n";
                 $message .= "Hallo Bapak/Ibu,\n";
                 $message .= "*" . $customer->nama_customer . "*,\n\n";
-                $message .= "No Invocie Tagihan : *". $invoice_real ."*\n";
+                $message .= "No Invocie Tagihan : *" . $invoice_real . "*\n";
                 $message .= "Tagihan Internet Anda jatuh tempo pada:\n\n";
                 $message .= "Bulan : *" . Carbon::parse($newDueDate)->format('F Y') . "*\n";
-                $message .= "Total Tagihan : *Rp ". number_format($belum, 0, ',', '.'). "*,-\n";
+                $message .= "Total Tagihan : *Rp " . number_format($belum, 0, ',', '.') . "*,-\n";
                 $message .= "Pembayaran maksimal : *" . Carbon::parse($newDueDate)->format('d F Y') . "*.\n\n";
                 $message .= "Bank Tersedia :\n";
-                $message .= "*BANK MANDIRI* : ". $banks[3]['nomor_akun_rekening']."\n";
-                $message .= "*BANK BCA* : ". $banks[2]['nomor_akun_rekening']."\n";
-                $message .= "*BANK BRI* : ". $banks[0]['nomor_akun_rekening']."\n";
-                $message .= "*BANK BNI* : ". $banks[1]['nomor_akun_rekening']."\n";
+                $message .= "*BANK MANDIRI* : " . $banks[3]['nomor_akun_rekening'] . "\n";
+                $message .= "*BANK BCA* : " . $banks[2]['nomor_akun_rekening'] . "\n";
+                $message .= "*BANK BRI* : " . $banks[0]['nomor_akun_rekening'] . "\n";
+                $message .= "*BANK BNI* : " . $banks[1]['nomor_akun_rekening'] . "\n";
                 $message .= "A/N *PUTUT WAHYUDI*\n\n";
                 $message .= "Segera lakukan pembayaran sebelum tanggal jatuh tempo, untuk mencegah isolir\n\n";
                 $message .= "Hormat kami\n*PT. Global Data Network*\nJl. Dinoyo Tenun No 109, RT.006/RW.003, Kel, Keputran, Kec, Tegalsari, Kota Surabaya, Jawa Timur 60265.\nPhone : 085731770730 / 085648747901\n\nhttps://billing.berdikari.web.id/tripay/transaction/$invoice_real/$belum";
