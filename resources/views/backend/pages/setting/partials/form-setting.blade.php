f95b627ce44ba35521061a722b837cfbd1e3d7f1
anandanajahudin <ahmadananda96@gmail.com>
2024-03-19 08:14:17 +0700

    update title on view setting

--- resources/views/backend/pages/setting/partials/form-setting.blade.php
+++ resources/views/backend/pages/setting/partials/form-setting.blade.php
@@ -7,9 +7,37 @@
             <div class="card-body">
                 <div class="row">
                     @foreach ($websetting as $websetting)
+                        @php
+                            if ($websetting->name == 'title') {
+                                $name = 'Judul';
+                            } elseif ($websetting->name == 'subtitle_text') {
+                                $name = 'Subjudul';
+                            } elseif ($websetting->name == 'site_currency') {
+                                $name = 'Mata Uang Situs';
+                            } elseif ($websetting->name == 'site_currency_symbol') {
+                                $name = 'Simbol Mata Uang Situs';
+                            } elseif ($websetting->name == 'company_name') {
+                                $name = 'Nama Perusahaan';
+                            } elseif ($websetting->name == 'company_address') {
+                                $name = 'Alamat Perusahaan';
+                            } elseif ($websetting->name == 'company_city') {
+                                $name = 'Kota';
+                            } elseif ($websetting->name == 'company_state') {
+                                $name = 'Provinsi';
+                            } elseif ($websetting->name == 'company_zipcode') {
+                                $name = 'Kode Pos';
+                            } elseif ($websetting->name == 'company_telephone') {
+                                $name = 'Telefon';
+                            } elseif ($websetting->name == 'company_email') {
+                                $name = 'Email';
+                            } else {
+                                $name = $websetting->name;
+                            }
+                        @endphp
+
                         <div class="col-sm-4">
                             <div class="form-group">
-                                <label class="form-label">{{ $websetting->name }}</label>
+                                <label class="form-label">{{ $name }}</label>
                                 <input type="text" name="name" class="form-control"
                                     value="{{ $websetting->value }}">
                             </div>
