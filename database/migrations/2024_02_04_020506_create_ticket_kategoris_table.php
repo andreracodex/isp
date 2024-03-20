37ac6d5a35208e6bebded24cf2a9108c9f878f69
anandanajahudin <ahmadananda96@gmail.com>
2024-03-20 08:16:36 +0700

    update migration and model TicketKategori

--- database/migrations/2024_02_04_020506_create_ticket_kategoris_table.php
+++ database/migrations/2024_02_04_020506_create_ticket_kategoris_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nama', 50);
             $table->integer('is_active')->default(1);
             $table->timestamps();
-
+            $table->softDeletes();
         });
     }
 
